43064f1306729ef273672f882ddd523c99125d01
amberm <nekosenpai@gmail.com>
2019-03-15 10:09:56 -0600

    added some test before making the endpoint... here goes

--- server.spec.js
+++ server.spec.js
@@ -11,3 +11,27 @@ describe('server stuff', () => {
     })
 
 })
+
+describe('getting the games', () => {
+
+    it('should get games', async () => {
+        const res = await request(server).get('/games');
+
+        expect(res.status).toBe(201);
+    })
+
+    it('should return json stuff', async () => {
+        const res = await request(server).get('/games');
+
+        expect(res.type).toBe('application/json');
+    })
+
+    // honestly I'm not real sure what a 3rd test should be
+    // on this endpoint, so here's a generic one...
+    it('should set testing environment', () => {
+        expect(process.env.DB_ENV).toBe('development');
+    });
+
+
+
+})
