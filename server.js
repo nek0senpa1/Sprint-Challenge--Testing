44bd78d3ba3f6715ccea4a289c5fb142075935e7
amberm <nekosenpai@gmail.com>
2019-03-15 10:16:48 -0600

    Look, Ma.  Code!

--- server.js
+++ server.js
@@ -4,9 +4,20 @@ const server = express();
 
 server.use(express.json());
 
+const BubbaHoTep = require('./funcs')
 
 server.get('/', (req, res) => {
   res.status(200).json('lookey here, it works');
 });
+
+server.get('/games', (rec,rez) => {
+    BubbaHoTep.getIt('games')
+    .then(stuffs => {
+        rez.status(201).json(stuffs)
+    })
+    .catch(err => {
+        rez.status(500).json(err)
+    })
+})
 
 module.exports = server;
